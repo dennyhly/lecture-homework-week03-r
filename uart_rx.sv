7a03f422a61ace41390dbcfe38c9f738c09b8022
dennyhly <dennyly626@gmail.com>
2026-04-20 03:03:31 -0700

    completed lab

--- uart_rx.sv
+++ uart_rx.sv
@@ -12,11 +12,8 @@ parameter BAUDRATE = 9600;
 
 // STATES: State of the state machine
 localparam DATA_BITS = 8;
-//localparam 
-//    INIT = 0, 
-//    IDLE = 1,
-//    RX_DATA = 2,
-//    STOP = 3;
+typedef enum logic [3:0]{INIT, IDLE, RX_DATA, STOP} state_t;
+state_t state = INIT;
 
 // CLOCK MULTIPLIER: Instantiate the clock multiplier
 logic uart_clk;
@@ -30,53 +27,35 @@ clock_mul #(
 
 // CROSS CLOCK DOMAIN: The rx_ready flag should only be set 1 one for one source 
 // clock cycle. Use the cross clock domain technique discussed in class to handle this.
-logic sync_ff1, sync_ff2, rx_sync, rx_ready_flag;
+logic sync_ff1, sync_ff2, rx_ready_flag;
 
-always @(posedge uart_clk) begin
+always @(posedge clk) begin
     sync_ff1 <= rx_ready_flag;
     sync_ff2 <= sync_ff1;
-    rx_sync <= sync_ff2;
-    rx_ready <= sync_ff2 ^ rx_sync;
+    rx_ready <= sync_ff1 ^ sync_ff2;
 end
 
 // STATE MACHINE: Use the UART clock to drive that state machine that receves a byte from the rx signal
 logic [2:0] rx_counter;
-typedef enum logic [3:0]{INIT, IDLE, RX_DATA, STOP} state_t;
-
-state_t state = INIT;
 
 always @(posedge uart_clk) begin
     case (state)
-        INIT: begin 
-            state = IDLE; rx_counter <= 0; 
-        end
+        INIT: begin state <= IDLE; rx_counter <= 0; rx_ready_flag <= 0; rx_data <= 0; end
 
         IDLE: begin 
-            if (rx == 0) begin 
-                state = RX_DATA; rx_counter <= 0;
-            end
-            else begin 
-                state <= IDLE;
-            end
+            if (rx == 0) begin state <= RX_DATA; rx_counter <= 0;end
+            else begin state <= IDLE; end
         end
 
         RX_DATA: begin
-            rx_data[rx_counter] <= rx_sync;
-            if (rx_counter < 7) begin
-                rx_counter <= rx_counter + 1; state <= RX_DATA;
-            end
-            else begin 
-                rx_counter <= 0; state <= STOP;
-            end
+            rx_data[rx_counter] <= rx;
+            if (rx_counter < DATA_BITS - 1) begin rx_counter <= rx_counter + 1; state <= RX_DATA; end
+            else begin rx_counter <= 0; state <= STOP; end
         end
 
         STOP: begin
-            if (rx_sync) begin
-                state <= IDLE; rx_ready_flag <= ~rx_ready_flag;
-            end
-            else begin 
-                state <= STOP;
-            end
+            if (rx) begin state <= IDLE; rx_ready_flag <= ~rx_ready_flag; end
+            else begin state <= STOP; end
         end
         default: state = INIT;
     endcase
