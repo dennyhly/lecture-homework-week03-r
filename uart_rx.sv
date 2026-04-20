e675a02169de53fc8a9daa2b83c025af970dd792
Denny Ly <117537833+dennyhly@users.noreply.github.com>
2026-04-20 09:39:23 +0000

    debugging uart

--- uart_rx.sv
+++ uart_rx.sv
@@ -12,17 +12,74 @@ parameter BAUDRATE = 9600;
 
 // STATES: State of the state machine
 localparam DATA_BITS = 8;
-localparam 
-    INIT = 0, 
-    IDLE = 1,
-    RX_DATA = 2,
-    STOP = 3;
+//localparam 
+//    INIT = 0, 
+//    IDLE = 1,
+//    RX_DATA = 2,
+//    STOP = 3;
 
 // CLOCK MULTIPLIER: Instantiate the clock multiplier
+logic uart_clk;
+clock_mul #(
+    .SRC_FREQ(SRC_FREQ),
+    .OUT_FREQ(BAUDRATE)
+) clk_mul (
+    .src_clk(clk),
+    .out_clk(uart_clk)
+);
 
 // CROSS CLOCK DOMAIN: The rx_ready flag should only be set 1 one for one source 
 // clock cycle. Use the cross clock domain technique discussed in class to handle this.
+logic sync_ff1, sync_ff2, rx_sync, rx_ready_flag;
+
+always @(posedge uart_clk) begin
+    sync_ff1 <= rx_ready_flag;
+    sync_ff2 <= sync_ff1;
+    rx_sync <= sync_ff2;
+    rx_ready <= sync_ff2 ^ rx_sync;
+end
 
 // STATE MACHINE: Use the UART clock to drive that state machine that receves a byte from the rx signal
+logic [2:0] rx_counter;
+typedef enum logic [3:0]{INIT, IDLE, RX_DATA, STOP} state_t;
+
+state_t state = INIT;
+
+always @(posedge uart_clk) begin
+    case (state)
+        INIT: begin 
+            state = IDLE; rx_counter <= 0; 
+        end
+
+        IDLE: begin 
+            if (rx == 0) begin 
+                state = RX_DATA; rx_counter <= 0;
+            end
+            else begin 
+                state <= IDLE;
+            end
+        end
+
+        RX_DATA: begin
+            rx_data[rx_counter] <= rx_sync;
+            if (rx_counter < 7) begin
+                rx_counter <= rx_counter + 1; state <= RX_DATA;
+            end
+            else begin 
+                rx_counter <= 0; state <= STOP;
+            end
+        end
+
+        STOP: begin
+            if (rx_sync) begin
+                state <= IDLE; rx_ready_flag <= ~rx_ready_flag;
+            end
+            else begin 
+                state <= STOP;
+            end
+        end
+        default: state = INIT;
+    endcase
+end
 
 endmodule
